455ae66ba98c9f598907149a117696937eb2d347
Aidan Lee <aidankglee@googlemail.com>
2022-08-05 12:11:14 +0100

    Integrated markers for companies and started the menu to hold the table of data.

--- kpmg-test/src/app/App.js
+++ kpmg-test/src/app/App.js
@@ -1,20 +1,178 @@
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { Button, Dropdown } from 'react-bootstrap';
 import Map from '../components/map/Map';
+import { Table } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExpand, faFilter, faCompress, faXmark } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const App = () => {
+
+  const [ companies, setCompanies ] = useState([]);
+  const [ menuFS, setMenuFS ] = useState(false);
+
+  const menuRef = useRef(null);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    getCompanies();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const getCompanies = async () => {
+
+      let data = await fetch('http://localhost:3000/companies.json');
+      data = await data.json();
+      setCompanies(data);
+
+  }
+
+  const toggleMenu = () => {
+    if (menuRef.current && mapRef.current) {
+
+      const menuOpen = menuRef.current.classList.contains('open');
+      console.log(mapRef.current.classList)
+
+      if (menuOpen) {
+
+        toggleFullscreen(true);
+        menuRef.current.classList.remove('open');
+        mapRef.current.classList.remove('menu-open');
+
+      } else {
+
+        menuRef.current.classList.add('open');
+        mapRef.current.classList.add('menu-open');
+
+      }
+
+    }
+  }
+
+  const toggleFullscreen = close => {
+    if (menuFS || close) {
+
+      setMenuFS(false);
+      menuRef.current.classList.remove('fullscreen');
+      mapRef.current.classList.remove('menu-fullscreen');
+
+    } else {
+
+      setMenuFS(true);
+      menuRef.current.classList.add('fullscreen');
+      mapRef.current.classList.add('menu-fullscreen');
+
+    }
+  }
 
   return (
     <div id="app">
 
-      <Map/>
+      <Map
+        companies={ companies }
+        handleToggleMenu={ toggleMenu }
+        mapRef={ mapRef }
+      />
 
-      <div id="controls">
+      <div id='menu' ref={ menuRef }>
 
+        <header id="menu-header">
 
+          <div className='left'>
+
+            <Dropdown>
+
+              <Dropdown.Toggle className="border" size="sm" variant="light">
+                <FontAwesomeIcon icon={ faFilter }/>
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+
+                <Dropdown.Header>Fees</Dropdown.Header>
+
+                <Dropdown.Header>Sector</Dropdown.Header>
+
+              </Dropdown.Menu>
+              
+            </Dropdown>
+
+          </div>
+
+          <div className='right'>
+
+            <Button
+              className='btn-light btn-sm border'
+              onClick={() => toggleFullscreen()}
+              >
+              <FontAwesomeIcon icon={ menuFS ? faCompress : faExpand }/>
+            </Button>
+
+            <Button
+              className='btn-light btn-sm border'
+              onClick={() => toggleMenu()}
+              >
+              <FontAwesomeIcon icon={ faXmark }/>
+            </Button>
+
+          </div>
+
+        </header>
+
+        <Table striped hover>
+
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Address</th>
+              <th>Sector</th>
+              <th>Fees</th>
+              <th>Stock Symbol</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              companies.map(company => {
+
+                return (
+                  <TableRow data={ company } key={ company.id }/>
+                )
+
+              })
+            }
+          </tbody>
+
+        </Table>
 
       </div>
 
     </div>
+  )
+
+}
+
+const TableRow = props => {
+
+  const { data } = useMemo(() => props, [props]);
+
+  const {
+    company,
+    address,
+    fees,
+    id,
+    sector,
+    stockSymbol
+  } = useMemo(() => data, [data]);
+
+  return (
+    <tr id={ id }>
+      <td>{ company }</td>
+      <td>{ address }</td>
+      <td>{ sector }</td>
+      <td>{ fees.amount } <b>{ fees.currency }</b> </td>
+      <td>{ stockSymbol }</td>
+    </tr>
   )
 
 }
--- kpmg-test/src/components/map/Map.js
+++ kpmg-test/src/components/map/Map.js
@@ -1,28 +1,124 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
-import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import L from 'leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import 'leaflet.markercluster/dist/leaflet.markercluster';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import 'leaflet/dist/leaflet.css';
 import './map.css';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTableList, faMagnifyingGlassPlus, faMagnifyingGlassMinus } from '@fortawesome/free-solid-svg-icons';
 import { Icon } from 'leaflet';
+import { useEffect, useMemo } from 'react';
 
 const Map = props => {
 
+    const { 
+        companies,
+        handleToggleMenu,
+        mapRef
+    } = useMemo(() => props, [props]);
+
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker 
-                position={[51.505, -0.09]}
-                icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}
-            >
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <div id="map" ref={ mapRef }>
+            <MapContainer
+                center={[51.505, -0.09]}
+                maxBounds={[[-90, -180], [90, 180]]}
+                maxBoundsViscosity={1}
+                minZoom={2}
+                zoom={3} 
+                zoomControl={false}
+                >
+                <Controls handleToggleMenu={ handleToggleMenu }/>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Companies data={ companies } />
+            </MapContainer>
+        </div>
     )
 
+}
+
+const Companies = props => {
+
+    const { data } = props;
+
+    const map = useMap();
+
+    useEffect(() => {
+
+        const markers = L.markerClusterGroup();
+        data.forEach(company => {
+            
+            const { latitude: lat, longitude: lng } = company.location;
+            
+            const {
+                address,
+                company: name,
+                // fees,
+                // id,
+                // sector,
+                // stockSymbol
+            } = company;
+
+            const marker = L.marker([lat, lng], {
+                icon: new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})
+            });
+
+            const popup = L.popup()
+                .setLatLng([lat, lng])
+                .setContent(
+                    `
+                    <p class="title">${name}</p>
+                    <p>${address}</p>
+                    `
+                )
+
+            marker.bindPopup(popup);
+        
+            markers.addLayer(marker);
+        })
+
+        map.addLayer(markers);
+
+    }, [data, map])
+
+}
+
+const Controls = props => {
+
+    const map = useMap();
+
+    const { handleToggleMenu } = useMemo(() => props, [props]);
+    
+    return (
+        <div id="controls">
+
+          <Button 
+            className='p-2 rounded-pill btn-light border' variant="primary"
+            onClick={() => map.zoomIn()}
+            >
+            <FontAwesomeIcon icon={faMagnifyingGlassPlus} />
+          </Button>
+
+          <Button
+            className='p-2 rounded-pill btn-light border' variant="primary"
+            onClick={() => map.zoomOut()}
+            >
+            <FontAwesomeIcon icon={faMagnifyingGlassMinus} />
+          </Button>
+
+          <Button
+            className='p-2 rounded-pill border' variant="primary"
+            onClick={() => handleToggleMenu()}
+            >
+            <FontAwesomeIcon icon={faTableList} />
+          </Button>
+
+        </div>
+    )
 }
 
 export default Map;
